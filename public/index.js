8a62b2b6ace9dd39ec8ed0fefa42646bf1a55ff1
Muhammad Murtala Abdullahi <murtalaabdullahi99@gmail.com>
2025-03-10 21:54:33 +0100

    fleshed out the better part of the website

--- public/index.js
+++ public/index.js
@@ -31,7 +31,7 @@ function toggleModal () {
   var navModal = document.getElementById('nav-modal')
   var menuIconLines = document.querySelectorAll('.menu-icon div')
   var cancelIcon = document.querySelector('.cancel-icon')
-  
+
   if (navModal.classList.contains('hidden')) {
     // Show modal
     navModal.classList.remove('hidden')
@@ -40,32 +40,51 @@ function toggleModal () {
     menuIconLines.forEach(function (line) {
       line.style.display = 'none' // Hide each line
     })
-    
+
     // Show cancel icon
     cancelIcon.style.display = 'inline-block'
-} else {
+  } else {
     // Hide modal
     navModal.classList.add('hidden')
-    
+
     // Show hamburger icon lines
     menuIconLines.forEach(function (line) {
-        line.style.display = '' // Reset display style
+      line.style.display = '' // Reset display style
     })
-    
+
     // Hide cancel icon
     cancelIcon.style.display = 'none'
-}
+  }
 }
 
-function turnOffModal(){
-    let navModal = document.getElementById('nav-modal')
-    let cancelIcon = document.querySelector('.cancel-icon')
-    var menuIconLines = document.querySelectorAll('.menu-icon div')
-    if(!navModal.classList.contains('hidden')){
-        navModal.classList.add('hidden')
-        cancelIcon.style.display = 'none';
-        menuIconLines.forEach(function (line){
-            line.style.display = ''
-        })
+function turnOffModal () {
+  let navModal = document.getElementById('nav-modal')
+  let cancelIcon = document.querySelector('.cancel-icon')
+  var menuIconLines = document.querySelectorAll('.menu-icon div')
+  if (!navModal.classList.contains('hidden')) {
+    navModal.classList.add('hidden')
+    cancelIcon.style.display = 'none'
+    menuIconLines.forEach(function (line) {
+      line.style.display = ''
+    })
+  }
+}
+function goToAppStore (path) {
+  if (path === 'playstore') {
+    if (window.location.href.includes('#')) {
+      const hashedIndex = window.location.href.indexOf('#')
+      const strippedPath = window.location.href.slice(0, hashedIndex)
+      window.location.href = strippedPath + 'appstore.html'
+    } else {
+      window.location.href = window.location.href + 'appstore.html'
     }
+  } else if (path === 'appstore') {
+    if (window.location.href.includes('#')) {
+      const hashedIndex = window.location.href.indexOf('#')
+      const strippedPath = window.location.href.slice(0, hashedIndex)
+      window.location.href = strippedPath + 'appstore.html'
+    } else {
+      window.location.href = window.location.href + 'appstore.html'
+    }
+  }
 }
